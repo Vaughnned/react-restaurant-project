df8c90f61e6e1c5f82d32031672426f1971b3a5e
Vaughn Nedderman <vaughn@nedderman.com>
2023-06-05 11:54:49 -0400

    initial commit

---
+++ src/MenuList.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import MenuItem from "./MenuItem";
+
+export default function MenuList() {
+  return (
+    <>
+      <MenuItem />
+    </>
+  );
+
+  // use local storage to hold the info for the orders
+  // react.dev website
+}
