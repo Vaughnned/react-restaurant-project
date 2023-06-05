76d649813c879d55b303afa8a77ff5cf02a9c15c
Vaughn Nedderman <vaughn@nedderman.com>
2023-06-05 16:48:09 -0400

    bug fix

--- src/Items.jsx
+++ src/Items.jsx
@@ -2,42 +2,42 @@ import { v4 as uuidv4 } from "uuid";
 
 export const INITIAL_MENU = [
   {
-    id: uuidv4(),
+    id: 1,
     name: "Cheese Burger",
     description: "asad",
     price: 10.99,
     src: "https://media.cheddars.com/en_us/images/product/smokehouse-burger-grilled-onions.jpg",
   },
   {
-    id: uuidv4(),
+    id: 2,
     name: "Hot Dog",
     description: "asad",
     price: 10.99,
     src: "https://cdn-p300.americantowns.com/img/article/ok-hot-dog-joints-1.jpg",
   },
   {
-    id: uuidv4(),
+    id: 3,
     name: "Chicken Fingers",
     description: "asad",
     price: 10.99,
     src: "https://aubreyskitchen.com/wp-content/uploads/2021/03/frozen-chicken-tenders-air-fryer.jpg",
   },
   {
-    id: uuidv4(),
+    id: 4,
     name: "Fries",
     description: "asad",
     price: 10.99,
     src: "https://airfryingfoodie.com/wp-content/uploads/2021/05/Air-Fryer-French-Fries-1.jpg",
   },
   {
-    id: uuidv4(),
+    id: 5,
     name: "Onion Rings",
     description: "asad",
     price: 10.99,
     src: "https://www.fastfoodmenuprices.com/wp-content/uploads/2020/09/Burger-King-Onion-Rings-Recipe.jpg",
   },
   {
-    id: uuidv4(),
+    id: 6,
     name: "Fried Pickles",
     description: "asad",
     price: 10.99,
